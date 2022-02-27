8aa421b491002da361d6fbdfbe52362bd7b8cf7e
Your Name <you@example.com>
2022-02-27 17:15:09 +0100

    final

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,58 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Form = () => {
+  const [moviesData, setMoviesData] = useState([]);
+  const [search, setSearch] = useState("code");
+  const [sortGoodBad, setSortGoodBad] = useState(null);
 
-    const [moviesData, setMoviesData] = useState([])
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=884686465de3838aebbea32efd9ef4b6&query=${search}&language=fr-FR`
+      )
+      .then((res) => setMoviesData(res.data.results));
+  }, [search]);
 
-    useEffect(() => {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=884686465de3838aebbea32efd9ef4b6&query=star&language=fr-FR')
-        .then((res) => setMoviesData(res.data.results));
-    }, [])
-
-    return (
-        <div className="form-component">
-            <div className="form-container">
-                <form>
-                    <input type="text" placeholder="Entrer le titre d'un film" id="search-input" />
-                    <input type="submit" value="Rechercher" />
-                </form>
-            </div>
-            <div className="btn-sort-container">
-                <div className="btn-sort" id="goodToBad">
-                    Top<span>→</span>
-                </div>
-                <div className="btn-sort" id="badToGood">
-                    Flop<span>→</span>
-                </div>
-            </div>
-            <div className="result">
-                {moviesData.map((movie) => <h2>{movie.title}</h2>)}
-            </div>
+  return (
+    <div className="form-component">
+      <div className="form-container">
+        <form>
+          <input
+            type="text"
+            placeholder="Entrer le titre d'un film"
+            id="search-input"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <input type="submit" value="Rechercher" />
+        </form>
+      </div>
+      <div className="btn-sort-container">
+        <div className="btn-sort" id="goodToBad" onClick={() => setSortGoodBad("goodToBad")} >
+          Top<span>→</span>
         </div>
-    );
+        <div className="btn-sort" id="badToGood" onClick={() => setSortGoodBad("badToGood")}>
+          Flop<span>→</span>
+        </div>
+      </div>
+      <div className="result">
+        {moviesData
+          .slice(0, 12)
+          .sort((a, b) => {
+              if(sortGoodBad){
+            if (sortGoodBad === "goodToBad") {
+              return b.vote_average - a.vote_average;
+            } else if (sortGoodBad === "badToGood"){
+                return a.vote_average - b.vote_average; 
+            }}
+          })
+          .map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Form;
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import Header from '../components/Header';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
 
 const UserList = (props) => {
-    return (
-        <div>        
-            <Header />
-            <h1>User list</h1>
-        </div>
-    );
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=884686465de3838aebbea32efd9ef4b6&language=fr-FR&external_source=imdb_id`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Coup de coeur <span>❤️</span>
+      </h2> 
+      <div className="result">
+          { listData.length > 0 ? listData.map((movie) => <Card movie={movie} key={movie.id} />) : <h2>Vide</h2>}
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
